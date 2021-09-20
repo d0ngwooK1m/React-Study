754b47fe659316443dc0c167f9b8ade90c739799
d0ngwook1m <dongwookim.dev@gmail.com>
2021-09-20 23:29:26 +0900

    리액트 1주차 공부

---
+++ week-1/src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import React from "react";
+import './App.css';
+
+function App() {
+  const styles = {
+    border: "1px solid #eee",
+    padding: "20px",
+    width: "200px",
+    margin: "30px auto", 
+  };
+  return (
+    <div className="App">
+      <div style={styles}>
+      <h1 style={{color: "green"}}>안녕하세요!</h1>
+      <hr/>
+      <p style={{textAlign: "left"}}>이름을 입력해주세요.</p>
+      <input type="text" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
